bb117bfc2e3e743dfb50f092ac19d97ad5e4c8e3
chriswdmoore <chriswdmoore@gmail.com>
2025-10-30 23:36:57 +1100

    2-3 - added read file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as book:
+        book_text = book.read()
+        return book_text
+    
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
