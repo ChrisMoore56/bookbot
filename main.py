1148ad5ead81a88a8b7323ebaa99b5d6e6ce36f0
chriswdmoore <chriswdmoore@gmail.com>
2025-11-02 00:14:54 +1100

    2-6 - Added Character count

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import get_book_text, count_words
+from stats import get_book_text, count_words, count_characters
 
 def main():
     path = "./books/frankenstein.txt"
+    
     word_count = count_words(path)
     message = f"Found {word_count} total words"
     print(message)
+
+    print(count_characters(path))
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,13 @@ def count_words(path):
     split_text = get_book_text(path).split()
     word_count = len(split_text)
     return word_count
+
+def count_characters(path):
+    character_count = {}
+    book = get_book_text(path).lower()
+    for character in book:
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+    return character_count
