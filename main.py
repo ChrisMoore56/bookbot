06a86105f865b237825e79c9ba324d80d35794ab
chriswdmoore <chriswdmoore@gmail.com>
2025-11-01 23:52:53 +1100

    2-4 - added word count

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def get_book_text(path):
     with open(path) as book:
         book_text = book.read()
         return book_text
-    
+
+def count_words(path):
+    split_text = get_book_text(path).split()
+    word_count = len(split_text)
+    return word_count
+
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    word_count = count_words("./books/frankenstein.txt")
+    message = f"Found {word_count} total words"
+    print(message)
 
 main()
